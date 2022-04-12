28c838c804ba899d1b57e92a5b442711d194de7a
Patrice Ferlet <metal3d@gmail.com>
2022-04-12 15:51:24 +0200

    Propose plotting widget to create line graph
    Maybe we should name it "chart" or "plot"

---
+++ cmd/graph_demo/main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	xwidget "fyne.io/x/fyne/widget"
+)
+
+func main() {
+	n := 4
+	app := app.New()
+	w := app.NewWindow("Graphs")
+
+	// to pause the "animation"
+	pause := make([]bool, n)
+
+	graphWidgets := make([]*xwidget.Graph, n)
+	graphBoxes := make([]fyne.CanvasObject, n)
+	datas := make([][]float32, n)
+
+	// create n graphs
+	for i := range graphWidgets {
+		graphWidgets[i] = xwidget.NewGraph(nil)
+		// Set a title for the graph, use nice Border layout
+		graphBoxes[i] = container.NewBorder(
+			widget.NewLabelWithStyle(fmt.Sprintf("Graph %d", i+1), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+			nil, nil, nil,
+			graphWidgets[i])
+	}
+
+	for j, g := range graphWidgets {
+		// fill the data with random values
+		datas[j] = make([]float32, 64)
+		for k := range datas[j] {
+			datas[j][k] = rand.Float32() * 50
+		}
+		g.SetData(datas[j])
+
+		// we will play with mouse event and use drawing zone
+		func(j int, g *xwidget.Graph) {
+			g.OnMouseIn = func(e *desktop.MouseEvent) {
+				pause[j] = true
+			}
+
+			g.OnMouseMoved = func(e *desktop.MouseEvent) {
+				OnMouseMoved(g, e)
+			}
+
+			g.OnMouseOut = func() {
+				pause[j] = false
+
+				// cleanup the drawing zone
+				drawZone := g.GetDrawable()
+				drawZone.Objects = []fyne.CanvasObject{}
+				g.Refresh()
+			}
+		}(j, g)
+	}
+
+	// Let's change the graph colors
+	graphWidgets[1].GetOptions().StrokeColor = theme.PrimaryColor()
+	graphWidgets[2].GetOptions().StrokeColor = theme.FocusColor()
+	graphWidgets[2].GetOptions().FillColor = theme.PressedColor()
+
+	go func() {
+		// Contiuously update the data
+
+		// remove the first data point and add a new one each 500ms
+		lock := make([]sync.Mutex, n)
+		for range time.Tick(500 * time.Millisecond) {
+			for i := range datas {
+				if pause[i] {
+					continue
+				}
+				lock[i].Lock()
+				datas[i] = append(datas[i][1:], rand.Float32()*50)
+				graphWidgets[i].SetData(datas[i])
+				lock[i].Unlock()
+			}
+		}
+	}()
+
+	// make a sinusoidal graph
+	sinus := xwidget.NewGraph(nil)
+
+	// set the number of value to plot
+	const nx = 1024
+	// set the y values slice
+	siny := make([]float32, nx)
+
+	for i := range [nx]int{} {
+		siny[i] = float32(math.Sin(float64(i) / 100)) // devide per 100 to get a smooth curve
+	}
+	sinus.SetData(siny)
+
+	// build the UI
+	grid := container.NewGridWithColumns(2, graphBoxes...)
+	grid.Add(sinus)
+
+	md := widget.NewLabel(`
+>> Graphs Demo
+
+This is a simple example of what you can do with graphs.
+Pass mouse on the dynamic graphs to see some
+"OnMouseMoved" behaviors.
+
+On the left, it's a simple sinusoidal function drawn.
+    `)
+	grid.Add(md)
+
+	w.SetContent(grid)
+	w.Resize(fyne.NewSize(580, 340))
+	w.ShowAndRun()
+}
+
+// This function is called when the mouse is moved on the graph. It creates 2 lines + on circle and a text to display the value at the mouse position.
+func OnMouseMoved(g *xwidget.Graph, e *desktop.MouseEvent) {
+	// get the value of data at the mouse position
+	val, curvePos := g.GetDataPosAt(e.Position)
+
+	// prepare the vertical verticalLine to display
+	lineColor := theme.DisabledColor()
+	verticalLine := canvas.NewLine(lineColor)
+	verticalLine.Position1 = fyne.NewPos(curvePos.X, 0)
+	verticalLine.Position2 = fyne.NewPos(curvePos.X, g.Size().Height)
+
+	horizontalLine := canvas.NewLine(lineColor)
+	horizontalLine.Position1 = fyne.NewPos(0, curvePos.Y)
+	horizontalLine.Position2 = fyne.NewPos(g.Size().Width, curvePos.Y)
+
+	// place a circle on the curvePos
+	circle := canvas.NewCircle(theme.ForegroundColor())
+	circle.Resize(fyne.NewSize(theme.TextSize()*.5, theme.TextSize()*.5))
+	circle.Move(curvePos.Subtract(fyne.NewPos(
+		circle.Size().Height/2, circle.Size().Width/2,
+	)))
+
+	// display the value over the circle
+	text := canvas.NewText(fmt.Sprintf("%.02f", val), theme.ForegroundColor())
+	text.TextSize = theme.TextSize() * 0.7
+	text.Move(curvePos.Add(fyne.NewPos(
+		text.TextSize*1.5, -text.TextSize*0.5,
+	)))
+
+	// then add line, circle and text to the graph
+	drawZone := g.GetDrawable()
+	drawZone.Objects = []fyne.CanvasObject{verticalLine, horizontalLine, circle, text}
+	g.Refresh()
+
+}
---
+++ widget/graph_test.go
@@ -0,0 +1,205 @@
+package widget
+
+import (
+	"image"
+	"image/color"
+	"testing"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/test"
+	"fyne.io/fyne/v2/theme"
+	"github.com/stretchr/testify/assert"
+)
+
+func createGraph() *Graph {
+	return NewGraph(nil)
+}
+
+func createGraphWithOptions() *Graph {
+	return NewGraph(&GraphOpts{
+		StrokeColor: color.RGBA{0x11, 0x22, 0x33, 255},
+		FillColor:   color.RGBA{0x44, 0x55, 0x66, 255},
+		StrokeWidth: 5,
+	})
+}
+
+func makeRasterize(win fyne.Window, graph *Graph) image.Image {
+	win.Resize(fyne.NewSize(500, 300))
+	img := graph.rasterize(int(graph.Size().Width), int(graph.Size().Height))
+	return img
+}
+
+func assertSize(t *testing.T, img image.Image, graph *Graph) {
+	assert.Greater(t, img.Bounds().Size().X, 0)
+	assert.Greater(t, img.Bounds().Size().Y, 0)
+	assert.Equal(t, img.Bounds().Size().X, int(graph.Size().Width))
+	assert.Equal(t, img.Bounds().Size().Y, int(graph.Size().Height))
+}
+
+func TestGraph_Creation(t *testing.T) {
+	graph := createGraph()
+
+	win := test.NewWindow(graph)
+	win.Resize(fyne.NewSize(500, 300))
+	defer win.Close()
+
+	assert.Equal(t, len(graph.data), 0)
+	assert.Equal(t, graph.opts.StrokeColor, theme.ForegroundColor())
+	assert.Equal(t, graph.opts.FillColor, theme.DisabledButtonColor())
+	assert.Equal(t, graph.opts.StrokeWidth, float32(1))
+
+	data := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	graph.SetData(data)
+	assert.Equal(t, len(graph.data), len(data))
+
+	// rasterize should be called
+	assert.NotEqual(t, graph.image, nil)
+}
+
+func TestGraph_CreationWithOptions(t *testing.T) {
+	graph := createGraphWithOptions()
+
+	win := test.NewWindow(graph)
+	win.Resize(fyne.NewSize(500, 300))
+	defer win.Close()
+
+	assert.Equal(t, graph.opts.StrokeColor, color.RGBA{0x11, 0x22, 0x33, 255})
+	assert.Equal(t, graph.opts.FillColor, color.RGBA{0x44, 0x55, 0x66, 255})
+	assert.Equal(t, graph.opts.StrokeWidth, float32(5))
+
+	data := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	graph.SetData(data)
+	assert.Equal(t, len(graph.data), len(data))
+
+	// rasterize should be called
+	assert.NotEqual(t, graph.image, nil)
+}
+
+func TestGraph_Rasterizer(t *testing.T) {
+	graph := createGraph()
+	data := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	graph.SetData(data)
+	win := test.NewWindow(graph)
+	win.Resize(fyne.NewSize(50, 70))
+	defer win.Close()
+
+	graph = createGraphWithOptions()
+	graph.Resize(fyne.NewSize(50, 70))
+	graph.SetData(data)
+	img := makeRasterize(win, graph)
+
+	assertSize(t, img, graph)
+
+}
+
+func TestGraph_RasterizerWithNegative(t *testing.T) {
+	graph := createGraph()
+	data := []float32{-1, -2, -3, -4, -5, -6, -7, -8, -9, -10}
+	graph.SetData(data)
+
+	win := test.NewWindow(graph)
+	win.Resize(fyne.NewSize(500, 300))
+	defer win.Close()
+
+	graph.Resize(fyne.NewSize(500, 300))
+
+	img := makeRasterize(win, graph)
+	assertSize(t, img, graph)
+
+	graph = createGraphWithOptions()
+	data = []float32{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4}
+	graph.SetData(data)
+	graph.Resize(fyne.NewSize(500, 300))
+	img = makeRasterize(win, graph)
+	assertSize(t, img, graph)
+}
+
+func TestGraph_WithNoData(t *testing.T) {
+	graph := createGraph()
+	win := test.NewWindow(graph)
+	win.Resize(fyne.NewSize(500, 300))
+	defer win.Close()
+
+	assert.Equal(t, len(graph.data), 0)
+	assert.Equal(t, graph.opts.StrokeColor, theme.ForegroundColor())
+	assert.Equal(t, graph.opts.FillColor, theme.DisabledButtonColor())
+	assert.Equal(t, graph.opts.StrokeWidth, float32(1))
+
+	// call rasterizer
+	img := makeRasterize(win, graph)
+	assertSize(t, img, graph)
+}
+
+func TestGraph_GetOpts(t *testing.T) {
+	opts := &GraphOpts{
+		StrokeColor: color.RGBA{0x11, 0x22, 0x33, 255},
+		FillColor:   color.RGBA{0x44, 0x55, 0x66, 255},
+		StrokeWidth: 5,
+	}
+	graph := NewGraph(opts)
+
+	assert.Equal(t, graph.opts, opts)
+	// in case of, check all fields
+	assert.Equal(t, graph.opts.StrokeColor, color.RGBA{0x11, 0x22, 0x33, 255})
+	assert.Equal(t, graph.opts.FillColor, color.RGBA{0x44, 0x55, 0x66, 255})
+	assert.Equal(t, graph.opts.StrokeWidth, float32(5))
+}
+
+func TestGraph_GetValAndCurvePos(t *testing.T) {
+	graph := createGraph()
+	win := test.NewWindow(graph)
+	win.Resize(fyne.NewSize(500, 300))
+	defer win.Close()
+
+	graph.CreateRenderer()
+	graph.Resize(fyne.NewSize(500, 300))
+
+	data := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	graph.SetData(data)
+	graph.rasterize(500, 300)
+
+	// Get the value at the center of the graph
+	x, y := graph.GetDataPosAt(fyne.NewPos(289, 200))
+	assert.Equal(t, float32(6), x)
+	assert.Equal(t, float32(250), y.X)
+	assert.Equal(t, float32(120), y.Y)
+}
+
+func TestGraph_Mouse(t *testing.T) {
+	control := 0
+
+	graph := createGraph()
+	graph.OnMouseIn = func(e *desktop.MouseEvent) {
+		control++
+	}
+	graph.OnMouseOut = func() {
+		control++
+	}
+	graph.OnMouseMoved = func(e *desktop.MouseEvent) {
+		control++
+	}
+	graph.OnTapped = func(e *fyne.PointEvent) {
+		control++
+	}
+
+	win := test.NewWindow(graph)
+	win.Resize(fyne.NewSize(500, 300))
+	defer win.Close()
+
+	graph.CreateRenderer()
+	graph.Resize(fyne.NewSize(500, 300))
+
+	data := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	graph.SetData(data)
+	graph.rasterize(500, 300)
+
+	// trigger all mouse events
+	graph.MouseIn(&desktop.MouseEvent{})
+	graph.MouseOut()
+	graph.MouseMoved(&desktop.MouseEvent{})
+	graph.Tapped(&fyne.PointEvent{})
+
+	assert.Equal(t, control, 4)
+
+}
